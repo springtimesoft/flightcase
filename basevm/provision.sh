04a92e94064c4a2cb6dfaefa998736e799256df2
arandomusername <vi.fri.martin@gmail.com>
2014-02-06 11:47:10 +1300

    Playbook that installs Symfony 2.4.1
    I couldn't use Composer for the nstallation because the Shared-Folder
    (on Windows) has some problems with symbolic links

--- basevm/provision.sh
+++ basevm/provision.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y ansible
 sudo ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/site.yml
+sudo ansible-playbook -i /vagrant/sym-playbook/hosts /vagrant/ansible/site.yml
